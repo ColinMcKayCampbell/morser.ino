271acb2cd58e01663f0aa4313d035e4e6b24a47c
ColinMcKayCampbell <ColinMcKayCampbell@users.noreply.github.com>
2017-03-29 19:52:18 +0100

    Cleaned up code and reduced overall file size.
    Size reduced from 7.64 kB to 4.4kB.

--- morser.ino
+++ morser.ino
@@ -27,389 +27,131 @@ void dash() {
   delay(dot_length);
 }
 
-void space() {
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(dot_length * 2);
-}
-
-void wordend() {
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(dot_length * 6);
-}
-
-void a() {
-  dot(); dash(); space();
-}
-
-void b() {
-  dash(); dot(); dot(); dot(); space();
-}
-
-void c() {
-  dash(); dot(); dash(); dot(); space();
-}
-
-void d() {
-  dash(); dot(); dot(); space();
-}
-
-void e() {
-  dot(); space();
-}
-
-void f() {
-  dot(); dot(); dash(); dot(); space();
-}
-
-void g() {
-  dash(); dash(); dot(); space();
-}
-
-void h() {
-  dot(); dot(); dot(); dot(); space();
-}
-
-void i() {
-  dot(); dot(); space(); space();
-}
-
-void j() {
-  dot(); dash(); dash(); dash(); space();
-}
-
-void k() {
-  dash(); dot(); dash(); space();
-}
-
-void l() {
-  dot(); dash(); dot(); dot(); space();
-}
-
-void m() {
-  dash(); dash(); space();
-}
-
-void n() {
-  dash(); dot(); space();
-}
-
-void o() {
-  dash(); dash(); dash(); space();
-}
-
-void p() {
-  dot(); dash(); dash(); dot(); space();
-}
-
-void q() {
-  dash(); dash(); dot(); dash(); space();
-}
-
-void r() {
-  dot(); dash(); dot(); space();
-}
-
-void s() {
-  dot(); dot(); dot(); space();
-}
-
-void t() {
-  dash(); space();
-}
-
-void u() {
-  dot(); dot(); dash(); space();
-}
-
-void v() {
-  dot(); dot(); dot(); dash(); space();
-}
-
-void w() {
-  dot(); dash(); dash(); space();
-}
-
-void x() {
-  dash(); dot(); dot(); dash(); space();
-}
-
-void y() {
-  dash(); dot(); dash(); dash(); space();
-}
-
-void z(){ 
-  dash(); dash(); dot(); dot(); space();
-}
-
-void zero() {
-  dash(); dash(); dash(); dash(); dash(); space();
-}
-
-void one() {
-  dot(); dash(); dash(); dash(); dash(); space();
-}
-
-void two() {
-  dot(); dot(); dash(); dash(); dash(); space();
-}
-
-void three() {
-  dot(); dot(); dot(); dash(); dash(); space();
-}
-
-void four() {
-  dot(); dot(); dot(); dot(); dash(); space();
-}
-
-void five() {
-  dot(); dot(); dot(); dot(); dot(); space();
-}
-
-void six() {
-  dash(); dot(); dot(); dot(); dot(); space();
-}
-
-void seven() {
-  dash(); dash(); dot(); dot(); dot(); space();
-}
-
-void eight() {
-  dash(); dash(); dash(); dot(); dot(); space();
-}
-
-void nine() {
-  dash(); dash(); dash(); dash(); dot(); space();
-}
-
-void period() {
-  dot(); dash(); dot(); dash(); dot(); dash(); space();
-}
-
-void comma() {
-  dash(); dash(); dot(); dot(); dash(); dash(); space();
-}
-
-void question() {
-  dot(); dot(); dash(); dash(); dot(); dot(); space();
-}
-
-void apostrophe() {
-  dot(); dash(); dash(); dash(); dash(); dot(); space();
-}
-
-void exclamation() {
-  dash(); dot(); dash(); dot(); dash(); dash(); space();
-}
-
-void slash() {
-  dash(); dot(); dot(); dash(); dot(); space();
-}
-
-void parenthesis_open() {
-  dash(); dot(); dash(); dash(); dot(); space();
-}
-
-void parenthesis_close() {
-  dash(); dot(); dash(); dash(); dot(); dash(); space();
-}
-
-void colon() {
-  dash(); dash(); dash(); dot(); dot(); dot(); space;
-}
-
-void semicolon() {
-  dash(); dot(); dash(); dot(); dash(); dot(); space();
-}
-
-void equal() {
-  dash(); dot(); dot(); dot(); dash(); space();
-}
-
-void plus() {
-  dot(); dash(); dot(); dash(); dot(); space();
-}
-
-void minus() {
-  dash(); dot(); dot(); dot(); dot(); dash(); space();
-}
-
-void quotation() {
-  dot(); dash(); dot(); dot(); dash(); dot(); space();
-}
-
-void at() {
-  dot(); dash(); dash(); dot(); dash(); dot(); space();
-}
-
-void error() {
-  e(); e(); e(); e(); e(); e(); e(); e(); space();
+void space(){
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(dot_length * 2);
 }
 
 void morse(String message) {
-  int message_length = message.length();
-  for (int it = 0; it < message_length; it++) {
-    char character = message[it];
-    if (character == 'a' || character == 'A') {
-      a();
+message.toLowerCase();
+
+for (int it = 0; it < message_length; it++) {
+		switch(message[it]){
+			case ' ':
+						digitalWrite(LED_BUILTIN, LOW);
+						delay(dot_length * 2);
+						break;
+
+			case 'a':	dot(); dash(); space(); break;
+						
+			case 'b':	dash(); dot(); dot(); dot(); space(); break;
+
+			case 'c':	dash(); dot(); dash(); dot(); space(); break;
+
+			case 'd':	dash(); dot(); dot(); space(); break;
+
+			case 'e':	dot(); space(); break;
+
+			case 'f':	dot(); dot(); dash(); dot(); space(); break;
+
+			case 'g':	dash(); dash(); dot(); space(); break;
+
+			case 'h':	dot(); dot(); dot(); dot(); space(); break;
+
+			case 'i':	dot(); dot(); space(); space(); break;
+
+			case 'j':	dot(); dash(); dash(); dash(); space(); break;
+
+			case 'k':	dash(); dot(); dash(); space(); break;
+
+			case 'l':	dot(); dash(); dot(); dot(); space(); break;
+
+			case 'm':	dash(); dash(); space(); break;
+
+			case 'n':	dash(); dot(); space(); break;
+
+			case 'o':	dash(); dash(); dash(); space(); break;
+
+			case 'p':	dot(); dash(); dash(); dot(); space(); break;
+
+			case 'q':	dash(); dash(); dot(); dash(); space(); break;
+
+			case 'r':	dot(); dash(); dot(); space(); break;
+
+			case 's':	dot(); dot(); dot(); space(); break;
+
+			case 't':	dash(); space(); break;
+
+			case 'u':	dot(); dot(); dash(); space(); break;
+
+			case 'v':	dot(); dot(); dot(); dash(); space(); break;
+
+			case 'w':	dot(); dash(); dash(); space(); break;
+
+			case 'x':	dash(); dot(); dot(); dash(); space(); break;
+
+			case 'y':	dash(); dot(); dash(); dash(); space(); break;
+
+			case 'z':	dash(); dash(); dot(); dot(); space(); break;
+
+			case '0':	dash(); dash(); dash(); dash(); dash(); space(); break;
+
+			case '1':	dot(); dash(); dash(); dash(); dash(); space(); break;
+
+			case '2':	dot(); dot(); dash(); dash(); dash(); space(); break;
+
+			case '3':	dot(); dot(); dot(); dash(); dash(); space(); break;
+
+			case '4':	dot(); dot(); dot(); dot(); dash(); space(); break;
+
+			case '5':	dot(); dot(); dot(); dot(); dot(); space(); break;
+
+			case '6':	dash(); dot(); dot(); dot(); dot(); space(); break;
+
+			case '7':	dash(); dash(); dot(); dot(); dot(); space(); break;
+
+			case '8':	dash(); dash(); dash(); dot(); dot(); space(); break;
+
+			case '9':	dash(); dash(); dash(); dash(); dot(); space(); break;
+
+			case '.':	dot(); dash(); dot(); dash(); dot(); dash(); space(); break;
+
+			case ',':	dash(); dash(); dot(); dot(); dash(); dash(); space(); break;
+
+			case '?':	dot(); dot(); dash(); dash(); dot(); dot(); space(); break;
+
+			case '\'':	dot(); dash(); dash(); dash(); dash(); dot(); space(); break;
+
+			case '!':	dash(); dot(); dash(); dot(); dash(); dash(); space(); break;
+
+			case '/':	dash(); dot(); dot(); dash(); dot(); space(); break;
+
+			case '(':	dash(); dot(); dash(); dash(); dot(); space(); break;
+
+			case ')':	dash(); dot(); dash(); dash(); dot(); dash(); space(); break;
+
+			case ':':	dash(); dash(); dash(); dot(); dot(); dot(); space; break;
+
+			case ';':	dash(); dot(); dash(); dot(); dash(); dot(); space(); break;
+
+			case '=':	dash(); dot(); dot(); dot(); dash(); space(); break;
+
+			case '+':	dot(); dash(); dot(); dash(); dot(); space(); break;
+
+			case '-':	dash(); dot(); dot(); dot(); dot(); dash(); space(); break;
+
+			case '"':	dot(); dash(); dot(); dot(); dash(); dot(); space(); break;
+
+			case '@':	dot(); dash(); dash(); dot(); dash(); dot(); space(); break;
+			
+			default: e(); e(); e(); e(); e(); e(); e(); e(); space();
+		}
     }
-    else if (character == 'b' || character == 'B') {
-      b();
-    }
-    else if (character == 'c' || character == 'C') {
-      c();
-    }
-    else if (character == 'd' || character == 'D') {
-      d();
-    }
-    else if (character == 'e' || character == 'E') {
-      e();
-    }
-    else if (character == 'f' || character == 'F') {
-      f();
-    }
-    else if (character == 'g' || character == 'G') {
-      g();
-    }
-    else if (character == 'h' || character == 'H') {
-      h();
-    }
-    else if (character == 'i' || character == 'I') {
-      i();
-    }
-    else if (character == 'j' || character == 'J') {
-      j();
-    }
-    else if (character == 'k' || character == 'K') {
-      k();
-    }
-    else if (character == 'l' || character == 'l') {
-      l();
-    }
-    else if (character == 'm' || character == 'M') {
-      m();
-    }
-    else if (character == 'n' || character == 'N') {
-      n();
-    }
-    else if (character == 'o' || character == 'O') {
-      o();
-    }
-    else if (character == 'p' || character == 'P') {
-      p();
-    }
-    else if (character == 'q' || character == 'Q') {
-      q();
-    }
-    else if (character == 'r' || character == 'R') {
-      r();
-    }
-    else if (character == 's' || character == 'S') {
-      s();
-    }
-    else if (character == 't' || character == 'T') {
-      t();
-    }
-    else if (character == 'u' || character == 'U') {
-      u();
-    }
-    else if (character == 'v' || character == 'V') {
-      v();
-    }
-    else if (character == 'w' || character == 'W') {
-      w();
-    }
-    else if (character == 'x' || character == 'X') {
-      x();
-    }
-    else if (character == 'y' || character == 'Y') {
-      y();
-    }
-    else if (character == 'z' || character == 'Z') {
-      z();
-    }
-    else if (character == '0') {
-      zero();
-    }
-    else if (character == '1') {
-      one();
-    }
-    else if (character == '2') {
-      two();
-    }
-    else if (character == '3') {
-      three();
-    }
-    else if (character == '4') {
-      four();
-    }
-    else if (character == '5') {
-      five();
-    }
-    else if (character == '6') {
-      six();
-    }
-    else if (character == '7') {
-      seven();
-    }
-    else if (character == '8') {
-      eight();
-    }
-    else if (character == '9') {
-      nine();
-    }
-    else if (character == ' ') {
-      wordend();
-    }
-    else if (character == '.') {
-      period();
-    }
-    else if (character == ',') {
-      comma();
-    }
-    else if (character == '?') {
-      question();
-    }
-    else if (character == '\'') {
-      apostrophe();
-    }
-    else if (character == '!') {
-      exclamation();
-    }
-    else if (character == '/') {
-      slash();
-    }
-    else if (character == '(') {
-      parenthesis_open();
-    }
-    else if (character == ')') {
-      parenthesis_close();
-    }
-    else if (character == ':') {
-      colon();
-    }
-    else if (character == ';') {
-      semicolon();
-    }
-    else if (character == '=') {
-      equal();
-    }
-    else if (character == '+') {
-      plus();
-    }
-    else if (character == '-') {
-      minus();
-    }
-    else if (character == '"') {
-      quotation();
-    }
-    else if (character == '@') {
-      at();
-    }    
-    else {
-      error();
-    }
-  }
-  wordend();
+
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(dot_length * 6);
+	return;
+
 }
 
 void loop() {    
